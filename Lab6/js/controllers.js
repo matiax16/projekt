6ac5337e8af8c13f01ab61407e1698ad3bfeb70e
matiax16 <szaturski14@gmail.com>
2022-06-04 14:35:25 +0200

    .
    .

---
+++ Lab6/js/controllers.js
@@ -0,0 +1,33 @@
+var portfolioApp = angular.module('portfolioApp',[]);
+
+portfolioApp.controller('GalleryListCtrl', function($scope)
+{
+    $scope.galleries = 
+    [
+        { 'title':'equalizer',
+        'when':'Listopad 2016',
+        'thumbnailUrl':'img/10.jpg'
+        },
+        { 'title':'kompresor',
+        'when':'Grudzień 2016',
+        'thumbnailUrl':'img/12.jpg'
+        },
+        { 'title':'reverb',
+        'when':'Styczeń 2017',
+        'thumbnailUrl':'img/11.jpg'
+        },
+        { 'title':'delay',
+        'when':'Luty 2017',
+        'thumbnailUrl':'img/13.jpg'
+        },
+        { 'title':'flanger',
+        'when':'Kwiecień 2017',
+        'thumbnailUrl':'img/14.jpg'
+        },
+        { 'title':'autotune pro',
+        'when':'Grudzień 2017',
+        'thumbnailUrl':'img/15.jpg'
+        }
+    ];
+    $scope.galleries.length;
+});
